8bcc89f4813a8fb460166204fcadc4a50e4e8703
alt-ROCK <56280184+aaltrock@users.noreply.github.com>
2021-10-25 03:01:46 +0100

    FN-27 Tweaked make template Sheet script to run in GCP console.

--- Phase 2/Data Pipeline/Stage_01_etl/make_dataflow_template.sh
+++ Phase 2/Data Pipeline/Stage_01_etl/make_dataflow_template.sh
@@ -1,8 +1,8 @@
 # Remove any existing template file created
-gsutil rm gs://src_fake_news_python_bs/template1
+gsutil rm gs://src_fake_news_bs/template1
 
 # Remove any temporary directory and content
-gsutil -m rm -r gs://src_fake_news_python_bs/tmp
+gsutil -m rm -r gs://src_fake_news_bs/tmp
 
 # Active Python virtual environment
 source /home/aaron_altrock/PycharmProjects/detect_fake_news_data_flow_bs/venv/bin/activate
@@ -17,14 +17,13 @@ python3 01_gcp_bucket_to_bigquery_dataflow.py --region europe-west2 \
 --runner DataflowRunner \
 --project fake-news-bs-detector \
 --staging_location=gs://fake_news_cleaned_json/staging \
---temp_location gs://src_fake_news_python_bs/tmp/ \
---template_location gs://src_fake_news_python_bs/template1 \
+--temp_location gs://src_fake_news_bs/tmp/ \
+--template_location gs://src_fake_news_bs/template1 \
 --requirements_file ./requirements_01_gcp_bucket_to_bigquery_dataflow.txt \
 --source_bucket_name src_fake_news_bs \
 --source_bucket_base_path to_add \
 --processed_bucket_success added \
 --bigquery_output_dataset fake_news \
 --bigquery_output_table src_fake_news \
---temp_location gs://src_fake_news_python_bs/tmp/ \
 --json_key_path ./fake-news-bs-detector-62e838f6b99c.json
 
